10e11480cb1fdeb49b6f783b8b64e36a3d340249
itssarah <is_abchice@esi.dz>
2023-10-05 09:46:44 +0000

    partie3

--- main.tf
+++ main.tf
@@ -17,6 +17,12 @@ resource "google_project_service" "cloud_build" {
   depends_on = [ google_project_service.ressource_manager ]
 }
 
+resource "google_project_service" "cloud_run" {
+  service = "run.googleapis.com"
+
+  depends_on = [ google_project_service.ressource_manager ]
+}
+
 resource "google_sql_database" "database" {
   name     = "wordpress"
   instance = "main-instance"
@@ -36,4 +42,50 @@ resource "google_artifact_registry_repository" "my-repo" {
   format        = "DOCKER"
 
   depends_on = [ google_project_service.artifact ]
+}
+
+resource "google_cloud_run_service" "default" {
+name     = "serveur-wordpress"
+location = "us-central1"
+
+template {
+   spec {
+      containers {
+        image = "us-central1-docker.pkg.dev/artifact-399517/website-tools/website-image"
+        ports {
+        container_port = 80
+        }
+     }
+   }
+
+   
+
+   metadata {
+      annotations = {
+            "run.googleapis.com/cloudsql-instances" = "artifact-399517:us-central1:main-instance"
+      }
+   }
+}
+
+traffic {
+   percent         = 100
+   latest_revision = true
+}
+}
+
+data "google_iam_policy" "noauth" {
+   binding {
+      role = "roles/run.invoker"
+      members = [
+         "allUsers",
+      ]
+   }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+   location    = google_cloud_run_service.default.location
+   project     = google_cloud_run_service.default.project
+   service     = google_cloud_run_service.default.name
+
+   policy_data = data.google_iam_policy.noauth.policy_data
 }
